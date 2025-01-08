30a46bb2bde9aa5774876bfa48ecf477ba006e83
sangwook <operawook@gmail.com>
2025-01-08 13:08:21 +0900

    [feat] #4 spm setting

---
+++ WithSuhyeon-iOS/WithSuhyeon-iOSUITests/WithSuhyeon_iOSUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  WithSuhyeon_iOSUITestsLaunchTests.swift
+//  WithSuhyeon-iOSUITests
+//
+//  Created by 우상욱 on 1/8/25.
+//
+
+import XCTest
+
+final class WithSuhyeon_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

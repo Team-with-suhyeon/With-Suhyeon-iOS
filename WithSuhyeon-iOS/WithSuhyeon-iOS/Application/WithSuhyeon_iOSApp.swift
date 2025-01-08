92ab8dcbc3dca0c6ca0e27abcc618166b1bd179b
sangwook <operawook@gmail.com>
2025-01-08 13:08:21 +0900

    [feat] #4 spm setting

---
+++ WithSuhyeon-iOS/WithSuhyeon-iOS/Application/WithSuhyeon_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WithSuhyeon_iOSApp.swift
+//  WithSuhyeon-iOS
+//
+//  Created by 우상욱 on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct WithSuhyeon_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WithSuhyeon-iOS/WithSuhyeon-iOSTests/WithSuhyeon_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  WithSuhyeon_iOSTests.swift
+//  WithSuhyeon-iOSTests
+//
+//  Created by 우상욱 on 1/8/25.
+//
+
+import Testing
+@testable import WithSuhyeon_iOS
+
+struct WithSuhyeon_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

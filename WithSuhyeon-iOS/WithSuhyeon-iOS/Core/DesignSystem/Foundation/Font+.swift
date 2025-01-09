3faa253fd9210c09d6c69f372599ee22d04f6ab9
sangwook <operawook@gmail.com>
2025-01-09 19:45:00 +0900

    [chore] #2 structure

---
+++ WithSuhyeon-iOS/WithSuhyeon-iOS/Core/DesignSystem/Foundation/Font+.swift
@@ -0,0 +1,12 @@
+//
+//  Font+.swift
+//  WithSuhyeon-iOS
+//
+//  Created by 우상욱 on 1/9/25.
+//
+
+import SwiftUI
+
+extension Font {
+    static let suitBold: Font = .custom("SUIT-Bold", size: <#T##CGFloat#>)
+}

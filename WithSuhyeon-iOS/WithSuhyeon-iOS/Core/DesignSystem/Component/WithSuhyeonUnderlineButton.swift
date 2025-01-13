b024814155b849109645515f6ade36d3e9fd611f
Yeji <mnbvcxzyj@gmail.com>
2025-01-14 01:43:38 +0900

    [feat] #15 feat underlineMiniButton component

---
+++ WithSuhyeon-iOS/WithSuhyeon-iOS/Core/DesignSystem/Component/WithSuhyeonUnderlineButton.swift
@@ -0,0 +1,47 @@
+//
+//  WithSuhyeonUnderlineButton.swift
+//  WithSuhyeon-iOS
+//
+//  Created by 김예지 on 1/14/25.
+//
+
+import SwiftUI
+
+struct WithSuhyeonUnderlineButton: View {
+    let title: String
+    let onTap: () -> Void
+    
+    var body: some View {
+        Button(action: {
+            onTap()
+        }) {
+            VStack(spacing: 4) {
+                Text(title)
+                    .font(.body03B)
+                    .foregroundColor(Color.gray500)
+                
+                Rectangle()
+                    .fill(Color.gray500)
+                    .frame(height: 1)
+                    .padding(.top, -2)
+            }
+            .frame(width: 25)
+        }
+        .buttonStyle(PlainButtonStyle())
+    }
+}
+
+struct WithSuhyeonUnderlineButtonTestView: View {
+    var body: some View {
+        HStack(spacing: 20) {
+            WithSuhyeonUnderlineButton(title: "보기") {}
+            
+            WithSuhyeonUnderlineButton(title: "보기") {}
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    WithSuhyeonUnderlineButtonTestView()
+}

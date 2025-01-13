fbdd35721d5f6c962af02b17b01c43bdbf9d7d37
sangwook <operawook@gmail.com>
2025-01-13 18:58:10 +0900

    [feat] #16 implement category item component

---
+++ WithSuhyeon-iOS/WithSuhyeon-iOS/Presentation/Gallery/Component/CategoryItem.swift
@@ -0,0 +1,109 @@
+//
+//  CategoryItem.swift
+//  WithSuhyeon-iOS
+//
+//  Created by 우상욱 on 1/13/25.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    let category: String
+    let scrollOffset: CGFloat
+    let isSelected: Bool
+    
+    init(category: String, scrollOffset: CGFloat, isSelected: Bool) {
+        self.category = category
+        self.scrollOffset = scrollOffset
+        let nospace = category.replacingOccurrences(of: " ", with: "")
+        self.afterTextWidth = CGFloat(nospace.count * 15)
+        self.isSelected = isSelected
+    }
+    
+    @State private var iconSize: CGFloat = 36
+    @State private var circleWidth: CGFloat = 60
+    @State private var circleHeight: CGFloat = 60
+    @State private var insideTextOpacity: Double = -5.0
+    @State private var outsideTextOpacity: Double = 1.0
+    @State private var textWidth: CGFloat = 0
+    @State private var textHeight: CGFloat = 26
+    @State private var allTextWidth: CGFloat = 40
+    @State private var allTextOpacity: Double = 1.0
+    @State private var afterTextWidth: CGFloat
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack(spacing: 0) {
+                if(category == "전체"){
+                    Text("ALL")
+                        .font(.body03B)
+                        .foregroundColor(isSelected ? .white : .gray700)
+                        .frame(width: max(allTextWidth, 0))
+                        .opacity(allTextOpacity)
+                } else {
+                    Image(icon: .icArchive24)
+                        .renderingMode(.template)
+                        .resizable()
+                        .frame(width: iconSize, height: iconSize)
+                        .foregroundColor(.white)
+                }
+                
+                Text(category)
+                    .lineLimit(1)
+                    .font(.body03SB)
+                    .foregroundColor(isSelected ? .white : .gray700)
+                    .frame(width: textWidth)
+                    .opacity(insideTextOpacity)
+            }
+            .frame(width: circleWidth, height: circleHeight)
+            .background(
+                RoundedRectangle(cornerRadius: circleHeight / 2)
+                    .fill(isSelected ? Color.primary500 : Color.gray50)
+            )
+            .onChange(of: scrollOffset) { newValue in
+                adjustAppearance(for: newValue)
+            }
+            Text(category)
+                .lineLimit(1)
+                .font(.body03B)
+                .foregroundColor(isSelected ? .primary600 : .gray400)
+                .frame(height: textHeight)
+                .opacity(outsideTextOpacity)
+        }
+        .frame(width: circleWidth)
+    }
+    
+    private func adjustAppearance(for newValue: CGFloat) {
+        let progress = min(max(0, -newValue / 100), 1)
+        let calculatedInsideTextOpacity = -5.0 + (progress * 6)
+        let calculatedOutsideTextOpacity = 1.0 - (progress * 1.17)
+        let calculatedAllTextOpacity = 1.0 - (progress * 3)
+        let calculatedAllTextWidth = 40 - (progress * 40)
+        let calculatedTextWidth = afterTextWidth * progress
+        let calculatedCircleHeight = 60 - (23 * progress)
+        let calculatedIconSize = 36 - (18 * progress)
+        let calculatedTextHeight = 26 - (progress * 26)
+        let calculatedCircleWidth: CGFloat
+        
+        if category == "전체" {
+            calculatedCircleWidth = 60 + ((afterTextWidth - 31) * progress)
+        } else {
+            calculatedCircleWidth = 60 + ((afterTextWidth - 18) * progress)
+        }
+        
+        withAnimation(.easeInOut(duration: 0.3)) {
+            self.circleWidth = calculatedCircleWidth
+            self.circleHeight = calculatedCircleHeight
+            self.iconSize = calculatedIconSize
+            self.textHeight = calculatedTextHeight
+        }
+        
+        DispatchQueue.main.async {
+            self.insideTextOpacity = calculatedInsideTextOpacity
+            self.outsideTextOpacity = calculatedOutsideTextOpacity
+            self.allTextOpacity = calculatedAllTextOpacity
+            self.allTextWidth = calculatedAllTextWidth
+            self.textWidth = calculatedTextWidth
+        }
+    }
+}

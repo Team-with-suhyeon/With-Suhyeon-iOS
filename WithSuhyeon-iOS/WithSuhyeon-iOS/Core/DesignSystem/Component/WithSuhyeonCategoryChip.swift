548c42d5fdba39ff38b350dea505225cd54b246a
sangwook <operawook@gmail.com>
2025-01-15 16:01:08 +0900

    [feat] #25 implement withsuhyeon category chip

---
+++ WithSuhyeon-iOS/WithSuhyeon-iOS/Core/DesignSystem/Component/WithSuhyeonCategoryChip.swift
@@ -0,0 +1,30 @@
+//
+//  WithSuhyeonCategoryChip.swift
+//  WithSuhyeon-iOS
+//
+//  Created by 우상욱 on 1/15/25.
+//
+
+import SwiftUI
+
+struct WithSuhyeonCategoryChip: View {
+    let title: String
+    
+    var body: some View {
+        Text(title)
+            .foregroundColor(
+                Color.primary500
+            )
+            .font(.caption01B)
+            .padding(.horizontal, 10)
+            .frame(height: 26)
+            .background(
+                Color.primary50
+            )
+            .cornerRadius(8)
+    }
+}
+
+#Preview {
+    WithSuhyeonCategoryChip(title: "바다")
+}

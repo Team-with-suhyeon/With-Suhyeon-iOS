c982b771c59bb454a8e24efe703685ed4a434268
sangwook <operawook@gmail.com>
2025-01-15 16:00:43 +0900

    [chore] #25 add alert state for withsuhyeon button state

--- WithSuhyeon-iOS/WithSuhyeon-iOS/Core/DesignSystem/Component/WithSuhyeonButton.swift
+++ WithSuhyeon-iOS/WithSuhyeon-iOS/Core/DesignSystem/Component/WithSuhyeonButton.swift
@@ -11,39 +11,62 @@ struct WithSuhyeonButton: View {
     let title: String
     let buttonState: WithSuhyeonButtonState
     let clickable: Bool
+    let icon: WithSuhyeonIcon?
     let onTapButton: () -> Void
     
-    init(title: String, buttonState: WithSuhyeonButtonState, clickable: Bool = true, onTapButton: @escaping () -> Void) {
+    init(title: String, buttonState: WithSuhyeonButtonState, clickable: Bool = true, icon: WithSuhyeonIcon? = nil, onTapButton: @escaping () -> Void) {
         self.title = title
         self.buttonState = buttonState
         self.clickable = clickable
+        self.icon = icon
         self.onTapButton = onTapButton
     }
     
     var body: some View {
         Button(action: onTapButton) {
-            Text(title)
-                .foregroundColor(
-                    buttonState == .disabled ? Color.gray400 : Color.white
-                )
-                .font(.body01B)
-                .frame(maxWidth: .infinity, maxHeight: 52)
+            HStack(spacing: 8) {
+                if let icon = icon {
+                    Image(icon: icon)
+                        .renderingMode(.template)
+                        .foregroundColor(
+                            buttonState == .disabled ? Color.gray400 : Color.white
+                        )
+                }
+                Text(title)
+                    .foregroundColor(
+                        buttonState == .disabled ? Color.gray400 : Color.white
+                    )
+                    .font(.body01B)
+            }
         }
+        .frame(maxWidth: .infinity, maxHeight: 56)
         .background(
-            buttonState == .disabled ? Color.gray200 : Color.primary500
+            getBackgroundColor()
         )
         .disabled(!clickable)
         .cornerRadius(16)
+    }
+    
+    private func getBackgroundColor() -> Color {
+        switch buttonState {
+            
+        case .disabled:
+            return Color.gray200
+        case .enabled:
+            return Color.primary500
+        case .alert:
+            return Color.red01
+        }
     }
 }
 
 #Preview {
     VStack {
         HStack(spacing: 0){
-            WithSuhyeonButton(title: "버튼", buttonState: .disabled, onTapButton: {})
+            WithSuhyeonButton(title: "버튼", buttonState: .disabled, icon: .icDownload24 ,onTapButton: {})
                 .padding(.leading, 8)
                 .padding(.trailing, 4)
-            WithSuhyeonButton(title: "버튼", buttonState: .enabled, onTapButton: {})
+            WithSuhyeonButton(title: "버튼", buttonState: .alert, onTapButton: {})
                 .padding(.leading, 4)
                 .padding(.trailing, 8)
         }
--- WithSuhyeon-iOS/WithSuhyeon-iOS/Core/DesignSystem/Foundation/WithSuhyeonButtonState.swift
+++ WithSuhyeon-iOS/WithSuhyeon-iOS/Core/DesignSystem/Foundation/WithSuhyeonButtonState.swift
@@ -10,4 +10,5 @@ import Foundation
 enum WithSuhyeonButtonState {
     case disabled
     case enabled
+    case alert
 }

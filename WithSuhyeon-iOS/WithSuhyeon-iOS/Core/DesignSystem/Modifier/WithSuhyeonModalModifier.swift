1404ccc8ac71dc7f369350b77a37decb21b05e94
sangwook <operawook@gmail.com>
2025-01-17 00:35:35 +0900

    [mod] #24 modify withsuhyeon modal modifier

--- WithSuhyeon-iOS/WithSuhyeon-iOS/Core/DesignSystem/Modifier/WithSuhyeonModalModifier.swift
+++ WithSuhyeon-iOS/WithSuhyeon-iOS/Core/DesignSystem/Modifier/WithSuhyeonModalModifier.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct WithSuhyeonModalModifier<ModalContent: View>: ViewModifier {
+    @State private var offsetY: CGFloat = 0
     let isPresented: Bool
     let isButtonEnabled: Bool
     let title: String
@@ -15,49 +16,77 @@ struct WithSuhyeonModalModifier<ModalContent: View>: ViewModifier {
     let onDismiss: () -> Void
     
     func body(content: Content) -> some View {
-        ZStack(alignment: .top) {
+        ZStack(alignment: .bottom) {
             content
             
             if isPresented {
-                ZStack(alignment: .top) {
-                    // 모달 핸들바
-                    RoundedRectangle(cornerRadius: 11)
-                        .fill(Color.gray200)
-                        .frame(width: 60, height: 4)
-                        .padding(.top, 12)
-                    
-                    VStack(alignment: .leading) {
-                        Text(title)
-                            .font(.title02B)
-                            .frame(alignment: .leading)
-                            .padding(.top, 44)
-                            .padding(.horizontal, 24)
-                            .padding(.bottom, 32)
-                        
-                        modalContent()
-                            .padding(.horizontal, 16)
-                        
-                        Button(action: {
-                            print("선택완료 버튼 선택")
-                            onDismiss()
-                        }) {
-                            Text("선택완료")
-                                .font(.body01B)
-                                .foregroundColor(isButtonEnabled ? Color.white : Color.gray400)
-                                .frame(maxWidth: .infinity, minHeight: 56)
-                                .background(isButtonEnabled ? Color.primary500 : Color.gray200)
-                                .cornerRadius(16)
-                        }
-                        .padding(.horizontal, 24)
-                        .padding(.top, 32)
-                        .padding(.bottom, 36)
-                        .disabled(!isButtonEnabled)
-                    }
-                    .background(Color.white)
-                    .cornerRadius(24)
-                }
+                Color.black.opacity(0.3)
+                    .onTapGesture { onDismiss() }
+                    .ignoresSafeArea(.container, edges: .top)
+                
+                modalView()
+                    .transition(.move(edge: .bottom))
+                    .offset(y: offsetY)
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                if value.translation.height > 0 {
+                                    offsetY = value.translation.height
+                                }
+                            }
+                            .onEnded { value in
+                                if value.translation.height > 100 {
+                                    onDismiss()
+                                }
+                                offsetY = 0
+                            }
+                    )
             }
         }
+        .animation(.easeInOut, value: isPresented)
+    }
+    
+    @ViewBuilder
+    private func modalView() -> some View {
+        VStack(alignment: .center) {
+            
+            RoundedRectangle(cornerRadius: 11)
+                .fill(Color.gray200)
+                .frame(width: 60, height: 4)
+                .padding(.top, 12)
+            
+            HStack{
+                Text(title)
+                    .font(.title02B)
+                    .frame(alignment: .leading)
+                    .padding(.top, 44)
+                    .padding(.horizontal, 24)
+                    .padding(.bottom, 32)
+                
+                Spacer()
+            }
+            
+            modalContent()
+                .padding(.horizontal, 16)
+            
+            Button(action: {
+                print("선택완료 버튼 선택")
+                onDismiss()
+            }) {
+                Text("선택완료")
+                    .font(.body01B)
+                    .foregroundColor(isButtonEnabled ? Color.white : Color.gray400)
+                    .frame(maxWidth: .infinity, minHeight: 56)
+                    .background(isButtonEnabled ? Color.primary500 : Color.gray200)
+                    .cornerRadius(16)
+            }
+            .padding(.horizontal, 24)
+            .padding(.top, 32)
+            .disabled(!isButtonEnabled)
+        }
+        .ignoresSafeArea()
+        .background(Color.white)
+        .withSuhyeonCornerRadius(24, corners: [.topLeft, .topRight])
     }
 }
 

e7ae4526f73d7a551073e9c24fc85db66ce36464
sangwook <operawook@gmail.com>
2025-01-19 23:59:56 +0900

    [feat] #53 implement WebSocketClient

---
+++ WithSuhyeon-iOS/WithSuhyeon-iOS/Core/Socket/WebSocketTargetType.swift
@@ -0,0 +1,13 @@
+//
+//  WebSocketTargetType.swift
+//  WithSuhyeon-iOS
+//
+//  Created by 우상욱 on 1/18/25.
+//
+
+import Foundation
+
+protocol WebSocketTargetType {
+    var baseURL: String { get }
+    var path: String { get }
+}

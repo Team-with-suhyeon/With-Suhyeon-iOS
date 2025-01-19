2885a8d254cabbac53331964e2b95c8cf4a77769
sangwook <operawook@gmail.com>
2025-01-20 00:07:37 +0900

    [chore] #53 routing

--- WithSuhyeon-iOS/WithSuhyeon-iOS/Core/Router/Router.swift
+++ WithSuhyeon-iOS/WithSuhyeon-iOS/Core/Router/Router.swift
@@ -27,7 +27,7 @@ public enum Destination: Hashable {
     case main(fromSignUp: Bool)
     case galleryUpload
     case galleryDetail(id: Int)
-    case chatRoom
+    case chatRoom(ownerRoomId: String, peerRoomId: String, ownerId: Int, peerId: Int, postId: Int, nickname: String)
     case blockingAccountManagement
     case myPost
     case setInterest

92286f148f5adff9b3613c986d0e58a88f076268
sangwook <operawook@gmail.com>
2025-01-20 00:03:08 +0900

    [feat] #53 implement domain entities

---
+++ WithSuhyeon-iOS/WithSuhyeon-iOS/Domain/Chat/Entity/Send.swift
@@ -0,0 +1,18 @@
+//
+//  Send.swift
+//  WithSuhyeon-iOS
+//
+//  Created by 우상욱 on 1/19/25.
+//
+
+import Foundation
+
+struct Send {
+    let ownerChatRoomId: String
+    let peerChatRoomId: String
+    let senderID: Int
+    let receiverID: Int
+    let postID: Int
+    let content: String
+    let type: String
+}

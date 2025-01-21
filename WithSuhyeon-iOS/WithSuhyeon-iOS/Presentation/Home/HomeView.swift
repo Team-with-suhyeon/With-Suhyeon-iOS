6ef9d7b26c81b94fdd0625fbefd13a70a7ef0bb8
sangwook <operawook@gmail.com>
2025-01-21 17:19:17 +0900

    [feat] #57 implement home feature

--- WithSuhyeon-iOS/WithSuhyeon-iOS/Presentation/Home/HomeView.swift
+++ WithSuhyeon-iOS/WithSuhyeon-iOS/Presentation/Home/HomeView.swift
@@ -6,17 +6,15 @@
 //
 
 import SwiftUI
+import Lottie
 
 struct HomeView : View {
-    @State var money: Int = 1000
-    let moneyTarget: Int = 4737
     
     @EnvironmentObject private var router: RouterRegistry
-    @State private var showBottomSheet: Bool = false
-    @State private var isButtonEnabled: Bool = true
+    @StateObject var feature = HomeFeature()
     
     init() {
-        UIScrollView.appearance().bounces = true
+        UIScrollView.appearance().decelerationRate = .init(rawValue: 1.0)
     }
     
     var body: some View {
@@ -25,8 +23,8 @@ struct HomeView : View {
                 gradient: Gradient(stops: [
                     .init(color: .homeGradientStart, location: 0.0),
                     .init(color: .homeGradientEnd, location: 0.3),
-                    .init(color: .homeGradientEnd, location: 0.66),
-                    .init(color: .white, location: 0.66),
+                    .init(color: .homeGradientEnd, location: feature.state.boundary),
+                    .init(color: .white, location: feature.state.boundary),
                     .init(color: .white, location: 1.0),
                 ]),
                 startPoint: .top,
@@ -43,29 +41,52 @@ struct HomeView : View {
                     Spacer()
                 }
                 
-                ScrollView {
+                ObservableScrollView(
+                    onPreferenceChange: { value in
+                        feature.send(.scrollChange(value))
+                    }
+                )
+                {
                     VStack(alignment: .leading, spacing: 0) {
-                        Text("지금까지 이만큼의\n수현이들의 여행이 이뤄졌어요!")
-                            .font(.body03SB)
-                            .foregroundColor(.gray25)
-                            .padding(.top, 37)
-                            .padding(.leading, 24)
-                        
-                        HStack(spacing: 0) {
-                            Text(money.formattedWithComma)
-                                .font(.heading00EB)
-                                .foregroundColor(.white)
-                                .padding(.top, 12)
-                                .padding(.leading, 24)
-                                .padding(.bottom, 20)
-                            
-                            Text("건")
-                                .font(.heading01SB)
-                                .foregroundColor(.white)
-                                .padding(.top, 12)
-                                .padding(.leading, 4)
-                                .padding(.bottom, 20)
+                        ZStack(alignment: .leading) {
+                            HStack {
+                                Spacer()
+                                LottieView(animation: .named("home"))
+                                    .configure{ lottieView in
+                                        lottieView.animationSpeed = 1
+                                        lottieView.loopMode = .loop
+                                    }
+                                    .playing()
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(maxWidth: 200)
+                            }
+                            VStack(alignment: .leading, spacing: 0) {
+                                Text("지금까지 이만큼의\n수현이들의 여행이 이뤄졌어요!")
+                                    .font(.body03SB)
+                                    .foregroundColor(.gray25)
+                                    .padding(.top, 37)
+                                    .padding(.leading, 24)
+                                
+                                HStack(spacing: 0) {
+                                    Text(feature.state.count.formattedWithComma)
+                                        .font(.heading00EB)
+                                        .foregroundColor(.white)
+                                        .padding(.top, 12)
+                                        .padding(.leading, 24)
+                                        .padding(.bottom, 20)
+                                    
+                                    Text("건")
+                                        .font(.heading01SB)
+                                        .foregroundColor(.white)
+                                        .padding(.top, 12)
+                                        .padding(.leading, 4)
+                                        .padding(.bottom, 20)
+                                }
+                            }
                         }
+                        
+                        
                         
                         
                         VStack(spacing: 0) {
@@ -82,17 +103,56 @@ struct HomeView : View {
                                     Image(icon: .icArrowRight16)
                                 }
                                 .padding(.vertical, 13)
+                                .onTapGesture {
+                                    feature.send(.tapSeeAllButton)
+                                }
                             }
                             .padding(.horizontal, 16)
                             .padding(.top, 17)
                             
                             ScrollView(.horizontal, showsIndicators: false) {
-                                HStack {
-                                    ZStack {
-                                        
+                                HStack(spacing: 8) {
+                                    ZStack(alignment: .topLeading) {
+                                        Image(image: .imgDoNotDisturb)
+                                        Text("쉿! 여행중엔\n방해 금지 모드")
+                                            .font(.body02B)
+                                            .foregroundColor(.black)
+                                            .padding(.top, 12)
+                                            .padding(.leading, 16)
                                     }
-                                    .frame(width: 202, height: 173)
+                                    .cornerRadius(20)
+                                    .onTapGesture {
+                                        feature.send(.tapTravelButton)
+                                    }
+                                    
+                                    ZStack(alignment: .topLeading) {
+                                        Image(image: .imgSeekAndHide)
+                                        Text("꼭꼭 숨어라\n나 오늘 집에 안간다")
+                                            .font(.body02B)
+                                            .foregroundColor(.white)
+                                            .padding(.top, 12)
+                                            .padding(.leading, 16)
+                                    }
+                                    .cornerRadius(20)
+                                    .onTapGesture {
+                                        feature.send(.tapHideAndSeekButton)
+                                    }
+                                    
+                                    ZStack(alignment: .topLeading) {
+                                        Image(image: .imgStudyCafe)
+                                        Text("엄마\n나 독서실이야")
+                                            .font(.body02B)
+                                            .foregroundColor(.white)
+                                            .padding(.top, 12)
+                                            .padding(.leading, 16)
+                                    }
+                                    .cornerRadius(20)
+                                    .onTapGesture {
+                                        feature.send(.tapStudyCafeButton)
+                                    }
                                 }
+                                .padding(.leading, 16)
+                                .padding(.trailing, 16)
                                 .padding(.top, 9)
                                 .padding(.bottom, 24)
                             }
@@ -118,7 +178,7 @@ struct HomeView : View {
                                             .renderingMode(.template)
                                             .foregroundColor(.gray300)
                                         
-                                        Text("강남/역삼/삼성")
+                                        Text(feature.state.location)
                                             .font(.body03SB)
                                             .foregroundColor(.gray400)
                                             .padding(.leading, 4)
@@ -127,25 +187,29 @@ struct HomeView : View {
                                 
                                 Spacer()
                                 
-                                Image(icon: .icArchive24)
-                                    .frame(width: 54, height: 54)
+                                Image(image: .imgMegaphone)
+                                    .offset(x: 0, y: +11)
+                                    .frame(height: 78)
                             }
+                            .padding(.top, 16)
                             .padding(.horizontal, 16)
-                            .padding(.top, 22)
-                            .padding(.bottom, 16)
                             
                             VStack(spacing: 12) {
-                                ForEach(0 ..< 3) { value in
+                                ForEach(feature.state.findSuhyeons.indices, id: \.self) { index in
                                     HomeFindSuhyeonContainer(
-                                        title: "서울역 수현이 구해요ㅠㅠ",
-                                        money: "5,000",
-                                        gender: .woman,
-                                        age: "20~24세",
-                                        timeStamp: "1월 25일 (토) 오후 2:30"
+                                        title: feature.state.findSuhyeons[index].title,
+                                        money: feature.state.findSuhyeons[index].money,
+                                        gender: feature.state.findSuhyeons[index].gender,
+                                        age: feature.state.findSuhyeons[index].age,
+                                        timeStamp: feature.state.findSuhyeons[index].timeStamp
                                     )
                                     .padding(.horizontal, 16)
+                                    .onTapGesture {
+                                        feature.send(.tapWithSuhyeonContainer(index))
+                                    }
                                 }
                             }
+                            .padding(.bottom, 16)
                         }
                         .background(Color.white)
                     }
@@ -156,21 +220,14 @@ struct HomeView : View {
         .frame(maxHeight: .infinity)
         .onAppear {
             UIScrollView.appearance().bounces = true
-            DispatchQueue.global(qos: .background).async {
-                while money < moneyTarget {
-                    usleep(1_000)
-                    DispatchQueue.main.async {
-                        if(money > moneyTarget - 38) {
-                            money = moneyTarget
-                        } else {
-                            money += 37
-                        }
-                    }
-                }
-            }
-            
-            if case let .main(fromSignUp) = router.currentDestination(), fromSignUp {
-                showBottomSheet = true
+            feature.updateCount()
+        }
+        .onReceive(feature.sideEffectSubject) { sideEffect in
+            switch sideEffect {
+            case .navigateToFindSuhyeon(let id):
+                router.navigateTab(to: .findSuhyeon)
+            case .navigateToGallery(let category):
+                router.navigateTab(to: .gallery)
             }
         }
         
@@ -179,4 +236,5 @@ struct HomeView : View {
 
 #Preview {
     HomeView()
+        .environmentObject(RouterRegistry())
 }

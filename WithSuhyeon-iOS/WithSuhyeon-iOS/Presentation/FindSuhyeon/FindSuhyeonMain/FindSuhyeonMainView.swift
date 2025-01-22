afa41c25f387f59bf73cf1f90e6531f631b51592
codeJiwon <loopy0107@sookmyung.ac.kr>
2025-01-22 17:25:33 +0900

    [feat] #35 add locationModalView and modify feature

--- WithSuhyeon-iOS/WithSuhyeon-iOS/Presentation/FindSuhyeon/FindSuhyeonMain/FindSuhyeonMainView.swift
+++ WithSuhyeon-iOS/WithSuhyeon-iOS/Presentation/FindSuhyeon/FindSuhyeonMain/FindSuhyeonMainView.swift
@@ -19,10 +19,8 @@ struct FindSuhyeonMainView: View {
         return formatter
     }()
     
-    @State private var dates: [Date] = []
-    
-    init() {
-        _dates = State(initialValue: (0..<7).compactMap { Calendar.current.date(byAdding: .day, value: $0, to: Date()) })
+    private var dates: [Date] {
+        (0..<7).compactMap { calendar.date(byAdding: .day, value: $0, to: today) }
     }
     
     var body: some View {
@@ -30,39 +28,8 @@ struct FindSuhyeonMainView: View {
             VStack {
                 headerView
                 scrollDateView
-                
-                ScrollView {
-                    LazyVStack {
-                        ForEach(feature.state.posts) { post in
-                            FindSuhyeonMainPostContainer(
-                                title: post.title,
-                                moneyView: {
-                                    HStack(spacing: 8) {
-                                        if let badgeState = post.badgeState {
-                                            postBadgeView(for: badgeState)
-                                        }
-                                        Text(post.money)
-                                            .font(.body01B)
-                                            .foregroundColor(.gray900)
-                                        Text("원")
-                                            .font(.body01SB)
-                                            .foregroundColor(.gray400)
-                                    }
-                                },
-                                gender: post.gender,
-                                age: post.age,
-                                timeStamp: post.timeStamp
-                            )
-                            .cornerRadius(24, corners: .allCorners)
-                            .onTapGesture {
-                                feature.send(.tapPost(post.id))
-                            }
-                        }
-                    }
-                    .padding(.all, 16)
-                }
-                .background(Color.gray100)
-                
+                postListView
+                    .background(Color.gray100)
                 writeButton
             }
             .onReceive(feature.sideEffectSubject) { sideEffect in
@@ -73,6 +40,35 @@ struct FindSuhyeonMainView: View {
                     print("Navigating to write screen")
                 }
             }
+            .withSuhyeonModal(
+                isPresented: feature.state.isLocationSelectPresented,
+                isButtonEnabled: feature.state.location.buttonEnable,
+                title: feature.state.alertType.title,
+                modalContent: {
+                    WithSuhyeonLocationSelect(
+                        withSuhyeonLocation: WithSuhyeonLocation.location,
+                        selectedMainLocationIndex: feature.state.location.selectedMainLocationIndex,
+                        selectedSubLocationIndex: feature.state.location.selectedSubLocationIndex
+                    ) { mainIndex, subIndex in
+                        feature.send(.selectLocation(main: mainIndex, sub: subIndex))
+                    }
+                },
+                onDismiss: {
+                    feature.send(.dismissBottomSheet)
+                },
+                onTapButton: {
+                    switch feature.state.alertType {
+                    case .locationSelect:
+                        if feature.state.location.dropdownState.toWithSuhyeonDropdownState() == .defaultState {
+                            feature.send(.tapBottomSheetButton)
+                            feature.send(.setLocationDropdownState(.isSelected))
+                        }
+                    default:
+                        break
+                    }
+                }
+            )
+            .padding()
         }
     }
     
@@ -87,14 +83,14 @@ struct FindSuhyeonMainView: View {
             .padding(.bottom, 15)
             .padding(.leading, 16)
             WithSuhyeonDropdown(
-                dropdownState: feature.state.dropdownState,
-                placeholder: "거창/함양/합천/산청/의령/창녕/함안",
+                dropdownState: feature.state.location.dropdownState.toWithSuhyeonDropdownState(),
+                placeholder: feature.state.location.tempSelectedLocation.isEmpty ? "거창/함양/합천/산청/의령/창녕/함안" : feature.state.location.tempSelectedLocation,
                 onTapDropdown: {
-                    feature.send(.tapDropdown)
+                    feature.send(.tapLocationDropdown(.locationSelect))
                 },
                 errorMessage: ""
             ) {
-                Text("거창/함양/합천/산청/의령/창녕/함안")
+                Text(feature.state.location.tempSelectedLocation.isEmpty ? "거창/함양/합천/산청/의령/창녕/함안" : feature.state.location.tempSelectedLocation)
                     .font(.body)
                     .foregroundColor(.gray900)
             }
@@ -103,14 +99,15 @@ struct FindSuhyeonMainView: View {
     
     private var scrollDateView: some View {
             ScrollView(.horizontal, showsIndicators: false) {
-                HStack() {
+                HStack {
+                    let totalDate = Date.distantPast
                     Button(action: {
-                        feature.send(.selectDate(today))
+                        feature.send(.selectDate(totalDate))
                     }) {
                         Text("전체")
                             .font(.body03B)
-                            .foregroundColor(feature.state.selectedDate == today ? .gray900 : .gray400)
-                            .padding(.all, 10)
+                            .foregroundColor(feature.state.selectedDate == totalDate ? .gray900 : .gray400)
+                            .padding(.horizontal, 10)
                     }
                     ForEach(dates, id: \.self) { date in
                         Button(action: {
@@ -119,13 +116,49 @@ struct FindSuhyeonMainView: View {
                             Text(dateFormatter.string(from: date))
                                 .font(.body03B)
                                 .foregroundColor(date == feature.state.selectedDate ? .gray900 : .gray400)
-                                .padding(.all, 10)
                         }
+                        .padding(.horizontal, 10)
                     }
                 }
+                .padding(.vertical, 10)
                 .padding(.horizontal, 8)
             }
         }
+    
+    private var postListView: some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(feature.state.posts) { post in
+                    FindSuhyeonMainPostContainer(
+                        title: post.title,
+                        moneyView: {
+                            HStack(spacing: 8) {
+                                if let badgeState = post.badgeState {
+                                    postBadgeView(for: badgeState)
+                                }
+                                Text(post.money)
+                                    .font(.body01B)
+                                    .foregroundColor(.gray900)
+                                Text("원")
+                                    .font(.body01SB)
+                                    .foregroundColor(.gray400)
+                            }
+                        },
+                        gender: post.gender,
+                        age: post.age,
+                        timeStamp: post.timeStamp
+                    )
+                    .cornerRadius(24)
+                    .onTapGesture {
+                        feature.send(.tapPost(post.id))
+                    }
+                }
+            }
+            .padding(.horizontal, 16)
+        }
+        .padding(.vertical, 16)
+    }
+    
     
     private var writeButton: some View {
         Button(action: {
@@ -149,6 +182,20 @@ struct FindSuhyeonMainView: View {
                     .fill(state == .expired ? Color.gray400 : Color.gray100)
                     .frame(height: 20)
             )
+    }
+    
+    private func locationModalView() -> some View {
+        VStack {
+            WithSuhyeonLocationSelect(
+                withSuhyeonLocation: WithSuhyeonLocation.location,
+                selectedMainLocationIndex: feature.state.location.selectedMainLocationIndex,
+                selectedSubLocationIndex: feature.state.location.selectedSubLocationIndex,
+                onTabSelected: { mainIndex, subIndex in
+                    feature.send(.selectLocation(main: mainIndex, sub: subIndex))
+                }
+            )
+        }
+        .padding()
     }
 }
 

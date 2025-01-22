c73471e61129d407e89eda78f0c0c8da62e80bac
sangwook <operawook@gmail.com>
2025-01-22 18:08:16 +0900

    [feat] #73 domain entities

---
+++ WithSuhyeon-iOS/WithSuhyeon-iOS/Domain/Home/Entity/Home.swift
@@ -0,0 +1,14 @@
+//
+//  Home.swift
+//  WithSuhyeon-iOS
+//
+//  Created by 우상욱 on 1/22/25.
+//
+
+import Foundation
+
+struct Home {
+    let count: Int
+    let region: String
+    let posts: [Post]
+}

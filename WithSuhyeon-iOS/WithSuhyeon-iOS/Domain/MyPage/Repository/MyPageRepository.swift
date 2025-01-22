60e0cc0c46278c46b9af7f8fccf516376f4cf5f4
sangwook <operawook@gmail.com>
2025-01-22 18:11:38 +0900

    [feat] #73 implement repositories

---
+++ WithSuhyeon-iOS/WithSuhyeon-iOS/Domain/MyPage/Repository/MyPageRepository.swift
@@ -0,0 +1,12 @@
+//
+//  MyPageRepository.swift
+//  WithSuhyeon-iOS
+//
+//  Created by 우상욱 on 1/22/25.
+//
+
+import Foundation
+
+protocol MyPageRepository {
+    func getUser(completion: @escaping (User) -> Void)
+}

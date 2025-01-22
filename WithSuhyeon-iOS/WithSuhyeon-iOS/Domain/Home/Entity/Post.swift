908eacc3d3c286414c806311054b3fb085dc849d
sangwook <operawook@gmail.com>
2025-01-22 18:08:16 +0900

    [feat] #73 domain entities

---
+++ WithSuhyeon-iOS/WithSuhyeon-iOS/Domain/Home/Entity/Post.swift
@@ -0,0 +1,17 @@
+//
+//  Post.swift
+//  WithSuhyeon-iOS
+//
+//  Created by 우상욱 on 1/22/25.
+//
+
+import Foundation
+
+struct Post {
+    let id: Int
+    let title: String
+    let price: Int
+    let age: String
+    let gender: Gender
+    let date: String
+}
---
+++ WithSuhyeon-iOS/WithSuhyeon-iOS/Domain/MyPage/Entity/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  WithSuhyeon-iOS
+//
+//  Created by 우상욱 on 1/22/25.
+//
+
+import Foundation
+
+struct User {
+    let nickname: String
+    let profileImageURL: String
+}
